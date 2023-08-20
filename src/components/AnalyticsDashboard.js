a05b06ca86182a7517b39710db84974cc7cf3044
JosephJohnson14694 <josephjohnson14694@yahoo.in>
2023-08-20 13:36:18 +1200

    Dashboard Css changes

--- src/components/AnalyticsDashboard.js
+++ src/components/AnalyticsDashboard.js
@@ -3,37 +3,44 @@ import "./Analytics.css";
 import { Layout } from "../Layout";
 
 const AnalyticsDashboard = () => {
+ 
+  const imageStyle = {
+		display: "block", // Set images to display as blocks to stack them vertically
+		margin: "10px 0 40px 0", // Add some margin between images
+		width: "60%", // Set the width of the images
+		height: "auto", // Automatically adjust the height while maintaining aspect ratio
+    marginLeft: "400px",
+    
+	};
   return (
-    <Layout>
-      <div className="dashboard-container">
-        <h1>Sentimental Analysis Dashboard</h1>
-        <div className="image-container">
-          <div className="image-left-item">
-            <img
-              src="/sentiment_analysis.gif"
-              alt="Chart"
-              className="image-chart"
-            />
-          </div>
-          <div className="image-item">
-            <img
-              src={"/new_sentiment_analysis_plot.png"}
-              alt="Accuracy"
-              className="image-accuracy"
-            />
-          </div>
-        </div>
-        <div className="image-accuracy">
-        <img
-          src={"/sentiment_analysis_trace_sim.gif"}
-          alt="Accuracy"
-          className="image-accuracy"
-        />
-      </div>
-      
-      </div>
-    </Layout>
-  );
+		<Layout>
+			<div className="dashboard-container">
+				<h1 style={{textAlign:"center"}}>Sentimental Analysis Dashboard</h1>
+				<div className="image-container">
+					<img
+						src="/sentiment_analysis.gif"
+						alt="Chart"
+						className="image-chart"
+						style={imageStyle}
+					/>
+
+					<img
+						src={"/new_sentiment_analysis_plot.png"}
+						alt="Accuracy"
+						className="image-accuracy"
+						style={imageStyle}
+					/>
+
+					<img
+						src={"/sentiment_analysis_trace_sim.gif"}
+						alt="Accuracy"
+						className="image-accuracy"
+						style={imageStyle}
+					/>
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export default AnalyticsDashboard;
