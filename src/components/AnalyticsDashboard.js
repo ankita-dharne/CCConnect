9f33f014bb676d315440587d7828544e774e2168
ANKITA DHARNE <ankydharne@gmail.com>
2023-08-20 13:01:29 +1200

    dashboard design

--- src/components/AnalyticsDashboard.js
+++ src/components/AnalyticsDashboard.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import './Analytics.css'
-import { Layout } from '../Layout';
+import React from "react";
+import "./Analytics.css";
+import { Layout } from "../Layout";
 
 const AnalyticsDashboard = () => {
   return (
     <Layout>
-    <div className="dashboard-container">
-      <h1>Sentimental Analysis Dashboard</h1>
-      <div className="image-item">
-          <img src='/sentiment_analysis_plot.png' alt="Chart" className="image-chart" />
+      <div className="dashboard-container">
+        <h1>Sentimental Analysis Dashboard</h1>
+        <div className="image-container">
+          <div className="image-left-item">
+            <img
+              src="/sentiment_analysis.gif"
+              alt="Chart"
+              className="image-chart"
+            />
+          </div>
+          <div className="image-item">
+            <img
+              src={"/new_sentiment_analysis_plot.png"}
+              alt="Accuracy"
+              className="image-accuracy"
+            />
+          </div>
         </div>
-        <div className="image-item">
-          <img src={'/model_accuracy.png'} alt="Accuracy" className="image-accuracy" />
-        </div>
-    </div>
+        <div className="image-accuracy">
+        <img
+          src={"/sentiment_analysis_trace_sim.gif"}
+          alt="Accuracy"
+          className="image-accuracy"
+        />
+      </div>
+      
+      </div>
     </Layout>
   );
 };
